fcc6dbdffa32bcaf336d650f601ac899bfcd5a95
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 18:06:25 -0300

    Create 016.py

---
+++ 016.py
@@ -0,0 +1,11 @@
+"""
+"Exercício 16 - Faça um Programa que peça dois números e imprima o maior deles.
+"""
+
+n1 = float(input("Digite o valor de n1: "))
+n2 = float(input("Digite o valor de n2: "))
+
+if n1 > n2:
+    print("O maior valor é: ",n1)
+else:
+    print("O maior valor é: ",n2)

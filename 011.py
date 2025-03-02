7032e8b30444ae1857a8bddb2fe84e7ef3c92c07
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 16:21:11 -0300

    Update 011.py

--- 011.py
+++ 011.py
@@ -1,8 +1,10 @@
-"Exercício 11 - Tendo como dado de entrada a altura (h) de uma pessoa,
-"construa um algoritmo que calcule seu peso ideal,"
-"utilizando as seguintes fórmulas:
-    "Para homens: (72.7*h) - 58"
-    "Para mulheres: (62.1*h) - 44.7"
+"""
+Exercício 11 - Tendo como dado de entrada a altura (h) de uma pessoa
+construa um algoritmo que calcule seu peso ideal,
+utilizando as seguintes fórmulas:
+    Para homens: (72.7*h) - 58
+    Para mulheres: (62.1*h) - 44.7
+    """
 
 nome = input("Digite seu nome: ")
 genero = input("Digite seu gênero (h ou m): ")

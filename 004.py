31290efda95438b7d520f8b5e577748b3995a14a
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 14:08:30 -0300

    Create 004.py

---
+++ 004.py
@@ -0,0 +1,7 @@
+"Exercício 4 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área"
+
+pi = 3.1416
+raio = float(input("Digite o raio do circulo: "))
+area = pi * (raio **2)
+
+print(f"A área de um circulo de raio {raio} é: {area:.2f} m2")

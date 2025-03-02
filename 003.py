f2afeeaf211834d223a2f266ba197f6cdc9d434f
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 13:58:36 -0300

    Update and rename exercicios de lógica em python to 003.py

---
+++ 003.py
@@ -0,0 +1,9 @@
+Exercícios de Lógica em Python
+
+Exercício 3
+
+n1 = int(input("Digite o valor de n1: "))
+n2 = int(input("Digite o valor de n2: "))
+
+soma = (n1+n2)
+print("A soma dos dois números é: ",soma)

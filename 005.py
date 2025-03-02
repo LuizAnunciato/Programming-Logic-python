7135e7bbb0e206f3c212abd79a4cb17cd192d200
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 14:14:09 -0300

    Create 005.py

---
+++ 005.py
@@ -0,0 +1,10 @@
+"Exercício 5 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário."
+
+lado1 = int(input("Digite o valor de lado 1: "))
+lado2 = int(input("Digite o valor de lado 2: "))
+
+area = (lado1 * lado2)
+dobro = (area * 2)
+
+print(f"A área de um quadrado com lados equivalentes a {lado1} e {lado2} ao dobro é: {dobro}")
+

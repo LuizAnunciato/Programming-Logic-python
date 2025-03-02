caaad78e46e219c76d13f6b29f6bdc90e0c2d4f0
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 15:15:33 -0300

    Create 009.py

---
+++ 009.py
@@ -0,0 +1,14 @@
+"Exercício 9 - Faça um Programa que peça 2 números inteiros e um número real"
+"Calcule e mostre:"
+    "o produto do dobro do primeiro com metade do segundo"
+    "a soma do triplo do primeiro com o terceiro"
+    "o terceiro elevado ao cubo"
+
+n1_inteiro = int(input("Digite um  número inteiro: "))
+n2_inteiro = int(input("Digite um número inteiro: "))
+n1_real = float(input("Digite um número real: "))
+
+a = n1_inteiro * 2 * (n2_inteiro / 2)
+b = n1_inteiro * 3 + n1_real
+c = n1_real ** 3
+

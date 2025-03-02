81fe5c57647a83e36f802a25f0647ac562e08f23
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 18:18:27 -0300

    Update 017.py

--- 017.py
+++ 017.py
@@ -1,2 +1,13 @@
+"""
 Faça um Programa que peça um valor e mostre na tela se o valor é
 positivo ou negativo.
+"""
+
+valor = int(input("Digite um número: "))
+
+if valor > 0:
+    print(f"O número {valor} é positivo")
+elif valor < 0:
+    print(f"O número {valor} é negativo")
+else:
+    print(f"O número {valor} é zero")

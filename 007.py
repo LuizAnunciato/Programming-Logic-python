24f9d37adc36d183244ef52c1f72cd4ff323758b
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 14:58:30 -0300

    Update 007.py

--- 007.py
+++ 007.py
@@ -1,1 +1,8 @@
+"Exercício 7 - Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius."
+"Fórmula: C = 5 * ((F-32) / 9)"
+
+F = float(input("Digite o valor da temperatura em (Fº) para ser transformada em (Cº): "))
+C = (5 * (F-32) / 9)
+
+print(f"{F} graus Fahrenheit em graus Celsius é: {C:.2f}")
 

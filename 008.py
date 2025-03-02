03e03f97e21635c0da95a8139c7c9f8ae8d687e6
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 15:03:27 -0300

    Update 008.py

--- 008.py
+++ 008.py
@@ -1,4 +1,5 @@
 "Exercício 8 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit"
+"Fórmula: C = ((C * 9/5) + 32)
 
 C = float(input("Digite a temperatura em Celsius: "))
 F = ((C * 9/5) + 32)

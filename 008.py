5d2ca6f1bb73ee0e4971f3c5fc514bdb5413a6cd
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 15:02:35 -0300

    Create 008.py

---
+++ 008.py
@@ -0,0 +1,6 @@
+"Exercício 8 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit"
+
+C = float(input("Digite a temperatura em Celsius: "))
+F = ((C * 9/5) + 32)
+
+print(f"{C}°C é igual a {F}°F")

46061a58a6ab2c39e223d98b50fa6bdb3c439be4
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 14:48:26 -0300

    Update 006.py

--- 006.py
+++ 006.py
@@ -1,1 +1,7 @@
+"Exercício 6 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês"
 
+salario_hora = float(input("Informe quanto você ganha por hora em (R$): "))
+horas_trabalhadas = int(input("Informe o número de horas trabalhadas no mês: "))
+salario_total = (salario_hora * horas_trabalhadas)
+
+print(f"Seu salário total nesse mês corresponde a: {salario_total}")

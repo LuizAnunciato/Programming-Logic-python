910a7d1fa239f8830af22702258ca11fa0c86d41
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-02 13:59:43 -0300

    002.py

---
+++ 002.py
@@ -0,0 +1,10 @@
+Exercício 2
+
+b1 = float(input("Digite o valor da sua nota b1: "))
+b2 = float(input("Digite o valor da sua nota b2: "))
+b3 = float(input("Digite o valor da sua nota b3: "))
+b4 = float(input("Digite o valor da sua nota b3: "))
+
+media = (b1 + b2 + b3 + b4) / 4
+print(f"Sua média é {media}")
+

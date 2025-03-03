3c9162032e2bd42c2ab88da3cd926090fc60de99
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-03 15:58:56 -0300

    Create 021.py

---
+++ 021.py
@@ -0,0 +1,14 @@
+"""
+Faça um Programa que leia três números e mostre o maior deles.
+"""
+
+numero_1 = int(input("Digite o primeiro número: "))
+numero_2 = int(input("Digite o segundo número: "))
+numero_3 = int(input("Digite o terceiro número: "))
+
+if numero_1 > numero_2 and numero_1 > numero_3:
+    print("O maior número é: ",numero_1)
+elif numero_2 > numero_1 and numero_2 > numero_3:
+    print("O maior número é: ",numero_2)
+else:
+    print("O maior número é: ",numero_3)

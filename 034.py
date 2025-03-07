5f35a4ab85d83e37a5e6e85f90830ee97eb927c4
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-07 14:54:19 -0300

    Update 034.py

--- 034.py
+++ 034.py
@@ -19,3 +19,17 @@ Para sacar a quantia de 399 reais, o programa fornece três notas de 100,
 uma nota de 50, quatro notas de 10, uma nota de 5 e quatro notas de 1.
 """
 
+saque = float(input("Digite o valor do saque: "))
+notas = [100,50,10,5,1]
+
+if saque < 10:
+    print("O saque mínimo é R$10,00")
+elif saque > 600:
+    print("O saque máximo é R$600,00")
+else:
+    print("Notas fornecidas: ")
+    for nota in notas:
+        qtd_notas = saque // nota
+        saque %= nota
+        if qtd_notas > 0:
+            print(f"{int(qtd_notas)} nota(s) de R${nota},00")

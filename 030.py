11486bbc751f5629387132e2eca7f0749332c87f
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-07 18:05:28 -0300

    Update 030.py

--- 030.py
+++ 030.py
@@ -1,5 +1,5 @@
 """
-Faça um Programa que peça os 3 lados de um triângulo.
+Exercício 30 - Faça um Programa que peça os 3 lados de um triângulo.
 O programa deverá informar se os valores podem ser um triângulo.
 Indique, caso os lados formem um triângulo, se o mesmo é:
     equilátero, isósceles ou escaleno.

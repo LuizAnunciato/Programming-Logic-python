475f3e51126173d22896d55c12f9724aa96ea23c
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-10 15:16:20 -0300

    Update 043.py

--- 043.py
+++ 043.py
@@ -3,3 +3,13 @@ Exercício 43 - Faça um programa que leia um nome de usuário e a sua senha e n
 igual ao nome do usuário, mostrando uma mensagem de erro e voltando a
 pedir as informações.
 """
+
+usuario = input("Digite seu nome de usuário: ")
+senha = input("Digite sua senha (apenas caracteres): ")
+
+while senha == usuario:
+    print("Erro, nome de usuário não pode ser semelhante a senha.")
+    usuario = input("Digite seu nome de usuário novamente: ")
+    senha = input("Digite sua senha novamente (apenas caracteres)")
+
+print("Acesso concedido.")

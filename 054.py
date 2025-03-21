e30c5243e45f261a653f36c14fde9c053bcfb63a
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-21 19:58:17 -0300

    Update 054.py

--- 054.py
+++ 054.py
@@ -3,3 +3,16 @@ Exercício 54 - Faça um programa que peça dois números, base e expoente,
 calcule e mostre o primeiro número elevado ao segundo número.
 Não utilize a função de potência da linguagem.
 """
+
+print("Vamos trabalhar com potenciação")
+print()
+
+base = int(input("Digite a base: "))
+expoente = int(input("Digite o expoente: "))
+
+resultado = 1
+
+for i in range(0,expoente):
+    resultado *= base
+
+print(f"{base} elevado a {expoente} = {resultado}")

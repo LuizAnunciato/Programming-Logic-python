db83231b2c8e4c4ea796677131031a750e829892
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-21 00:04:23 -0300

    Update 049.py

--- 049.py
+++ 049.py
@@ -1,3 +1,16 @@
 """
-Exercício 48 - Faça um programa que leia 5 números e informe a soma e a média dos números.
+Exercício 49 - Faça um programa que leia 5 números e informe a soma e a média dos números.
 """
+
+numeros = []
+soma = 0
+
+for i in range(1,6):
+    numero = int(input(f"Digite o {i}º número: "))
+    numeros.append(numero)
+    
+soma = sum(numeros)
+media = (soma / len(numeros))
+
+print(f"A soma de dos números é: {soma}")
+print(f"A média dos números é: {media}")

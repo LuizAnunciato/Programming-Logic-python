80df6c2c96f7f465071e7602c0121e29e042c090
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-20 21:33:02 -0300

    Create 046.py

---
+++ 046.py
@@ -0,0 +1,5 @@
+"""
+Exercício 46 - Altere o programa anterior permitindo ao usuário informar as populações e as
+taxas de crescimento iniciais. Valide a entrada e permita repetir a operação.
+"""
+

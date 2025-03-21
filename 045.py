a96254744b165e4bbec0237995ed2d71e9d2e350
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-20 21:32:04 -0300

    Update 045.py

--- 045.py
+++ 045.py
@@ -6,3 +6,18 @@ Exercício 45 - Faça um programa que leia e valide as seguintes informações:
     Sexo: 'f' ou 'm';
     Estado Civil: 's', 'c', 'v', 'd';
 """
+
+populacao_a = 80000
+populacao_b = 200000
+
+taxa_crescimento_a = 0.03
+taxa_crescimento_b = 0.015
+
+anos = 0
+
+while populacao_a < populacao_b:
+    populacao_a += taxa_crescimento_a * populacao_a
+    populacao_b += taxa_crescimento_b * populacao_b
+    anos += 1
+
+print("A quantidade de anos que a população A irá ultrapassar a populaçãio B é: ", anos)

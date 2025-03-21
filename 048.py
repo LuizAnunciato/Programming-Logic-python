b127acb37e1faeb2b6602f33a47618cd027d93d0
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-03-20 23:54:19 -0300

    Update 048.py

--- 048.py
+++ 048.py
@@ -1,3 +1,12 @@
 """
 Exercício 48 - Faça um programa que leia 5 números e informe o maior número.
 """
+
+numeros = []
+
+for i in range(1,6):
+    numero = int(input(f"Digite o {i}º número: "))
+    numeros.append(numero)
+
+maior_numero = max(numeros)
+print(maior_numero)   

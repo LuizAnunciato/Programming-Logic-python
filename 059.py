849aebfdd92e1802dd154eea7b89181269834a4b
Luiz Felipe Anunciato Nobre <mcluizspessoal@gmail.com>
2025-04-01 22:40:55 -0300

    Update 059.py

--- 059.py
+++ 059.py
@@ -3,3 +3,17 @@ Exercício 59 - Faça um programa que peça um número inteiro e determine se el
 número primo. Um número primo é aquele que é divisível somente por ele mesmo
 e por 1.
 """
+numero = int(input("Digite um número para verificar se é primo ou não: "))
+
+if numero <= 1:
+    print(f"{numero} não é um número primo.")
+else:
+    primo = True
+    for i in range(2, numero):
+        if numero % i:
+            primo = False
+            break
+    if primo:
+        print(f"{numero} é um número primo.")
+    else:
+        print(f"{numero} não é um número primo.")
